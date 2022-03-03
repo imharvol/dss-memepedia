b2f438cc6b354a89f1abb0c394dfb2d0d57b04ec
Santiago de la Torre Vicedo <sdlt6@gcloud.ua.es>
2022-03-03 14:59:48 +0100

    Creo que está todo

---
+++ database/seeders/EvaluationTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use DB;
+
+class EvaluationTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('Evaluation')->delete();
+        DB::table('Evaluation')->insert(['postDate' => Carbon::parse('04-07-2022'), 'comment' => 'Muy bueno jaja', 'rating' => '9.5/10']);
+        DB::table('Evaluation')->insert(['postDate' => Carbon::parse('10-08-2022'), 'comment' => 'nooo epicoo', 'rating' => '8/10']);
+
+    }
+}
