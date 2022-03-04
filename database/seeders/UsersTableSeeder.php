772d17a40f5e85eeb6468b34bc9860afd9d4377e
nvog1 <57539970+nvog1@users.noreply.github.com>
2022-03-04 16:43:55 +0100

    Migracion y Seed de User
    * added user attributes
    
    * migrated user table. fixed phpunit.xml
    
    * implemented users seeder
    
    * fixed user migration and seeder
    
    * fixed users migration and added registerDate. checked.

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+        DB::table('users')->insert(['name' => 'VanOlmen', 'email' => 'peepo@gmail.com', 
+                                    'password' => '123','nick' => 'peepo', 'surname' => 'Nico',
+                                    'registerDate' => Carbon::parse('4-03-2022')]);
+        DB::table('users')->insert(['name' => 'Sanchez', 'email' => 'rick@gmail.com', 
+                                    'password' => '234', 'nick' => 'rick', 'surname' => 'Matias',
+                                    'registerDate' => Carbon::parse('4-03-2022')]);
+    }
+}
