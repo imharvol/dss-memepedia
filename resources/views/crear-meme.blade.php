f2ba6031af2cff86d43a7483634a0cabdc2272dc
nvog1 <nvog1@gcloud.ua.es>
2022-04-05 20:58:59 +0200

    implemented subir meme view

--- resources/views/crear-meme.blade.php
+++ resources/views/crear-meme.blade.php
@@ -7,5 +7,22 @@
 
 @section('content')
 @parent
-<h1>Este Es el contenido de la página crear meme.</h1>
+<div class="rectanguloCrearMeme" align="center">
+    <div class="textoCrearMeme">
+    <span>Sube tu MEME</span>
+    </div>
+    <div class="lineaHorizontal1"></div>
+    <form action="/action_page.php">
+            <label class="label" for="tituloMeme">Título</label>
+            <input type="textbox" name="tituloMeme" id="tituloMeme" class="textbox" placeholder="Título" size="50" auto>
+            <label for="">Etiquetas</label>
+            <input type="textbox" name="etiquetasMeme" id="etiquetasMeme" class="textbox" placeholder="Etiquetas (separadas por comas)" size="50">  
+            <label>Subir imagen(formatos jpg, tif y png. Máx. 200kB) </label> 
+            <br>
+            <img src="url" alt="icono imagen"> 
+            <div class="botonInicio">
+                <input class="boton" type="submit" name="btnSubirMeme" id="btnSubirMeme" value="Subir">
+            </div>
+    </form>
+</div>
 @endsection
