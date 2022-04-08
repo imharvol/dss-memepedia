52a5a1cbbbe5882b12ca52dd7a972777fab99ffd
imharvol <imharvol@gmail.com>
2022-04-08 20:18:45 +0000

    Anadir ordenado y filtrado en /search

--- resources/views/meme-list.blade.php
+++ resources/views/meme-list.blade.php
@@ -8,7 +8,7 @@
 @section('content')
 @parent
 
-<h1>Lista de  Memes</h1>
+<h1>Lista de Memes</h1>
 
 <table>
   <tr>
@@ -18,16 +18,22 @@
     <th>Author Id</th>
     <th>Created At</th>
     <th>Updated At</th>
+    <th>Tags</th>
   </tr>
 
   @foreach ($memes as $meme)
   <tr>
-    <td>{{$meme->id}}</td>
+    <td><a href="{{route('meme.show', ['memeId' => $meme->id])}}">{{$meme->id}}</a></td>
     <td>{{$meme->name}}</td>
     <td>{{$meme->description}}</td>
-    <td>{{$meme->author->username}}</td>
+    <td><a href="{{route('search', ['q' => $meme->author->username])}}">{{$meme->author->username}}</a></td>
     <td>{{$meme->created_at}}</td>
     <td>{{$meme->updated_at}}</td>
+    <td>
+      @foreach ($meme->tags as $tag)
+      <span>&#60;<a href="{{route('search', ['q' => $tag->name])}}">{{$tag->name}}</a>&#62;</span>
+      @endforeach
+    </td>
   </tr>
   @endforeach
 </table>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -12,6 +12,23 @@
 <div class="search-container">
   <form action="">
     <input class="search-input" type="text" placeholder="Busqueda ..." name="q" id="q" value="{{$q}}">
+
+    <select name="filter" class="search-options">
+      <option value="">Sin filtrado</option>
+      <option value="name" {{$filter == 'name' ? 'selected' : null}}>Filtrar por nombre</option>
+      <option value="description" {{$filter == 'description' ? 'selected' : null}}>Filtrar por descripción</option>
+      <option value="author" {{$filter == 'author' ? 'selected' : null}}>Filtrar por autor</option>
+      <option value="tags" {{$filter == 'tags' ? 'selected' : null}}>Filtrar por tags</option>
+    </select>
+
+    <select name="sort" class="search-options">
+      <option value="new" {{$sort == 'new' ? 'selected' : null}}>Mas nuevos primero</option>
+      <option value="old" {{$sort == 'old' ? 'selected' : null}}>Mas viejos primero</option>
+      <option value="long" {{$sort == 'long' ? 'selected' : null}}>Mas largos primero</option>
+      <option value="short" {{$sort == 'short' ? 'selected' : null}}>Mas cortos primero</option>
+      <option value="alphabetical" {{$sort == 'alphabetical' ? 'selected' : null}}>Ordenar alfabeticamente</option>
+    </select>
+
     <button type="submit"><i class="fa fa-search"></i></button>
   </form>
 </div>
