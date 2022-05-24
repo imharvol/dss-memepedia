297a1e83a7743e8c5c643d645946db88528d345f
imharvol <imharvol@gmail.com>
2022-05-24 09:34:16 +0000

    Ranking funcional

--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -8,66 +8,60 @@
 @section('content')
 @parent
 
-    <div class="textoBienvenida">
-        <div class="MEMEpedia">
-            <scap>Ranking</scap>
-        </div>
-        <div>
-            <scap>los memes mejor valorados</scap>
-        </div>
+<div class="textoBienvenida">
+    <div class="MEMEpedia">
+        <scap>Ranking</scap>
     </div>
+    <div>
+        <scap>los memes mejor valorados</scap>
+    </div>
+</div>
 
-    <div class="clasificacion" align="center">
-        <button>Semana</button>
+<div class="clasificacion" align="center">
+    <a href="{{route('ranking', ['r' => 'week'])}}">
+        <button class="selected-button">Semana</button>
+    </a>
+    <a href="{{route('ranking', ['r' => 'month'])}}">
         <button>Mes</button>
+    </a>
+    <a href="{{route('ranking', ['r' => 'year'])}}">
         <button>Año</button>
+    </a>
+    <a href="{{route('ranking', ['r' => 'all'])}}">
         <button>HISTÓRICO</button>
-    </div>
-    
-    <div class="subRectangulo">
-        <div class="rectanguloPublicacion layout">
-            <div class="sidebar" style="margin-top: 7vh">
-                <button class="botones"><span class="bx bx-up-arrow-alt"><i></i></span></button>
-                <div class="upvotes">874k</div>
-                <button class="botones"><span class="bx bx-down-arrow-alt"><i></i></span></button>
-            </div>
-            <div class="body">
-                <scap class="autor">AutorRandom123</scap> <scap class="fecha">17/03/2022</scap>
-                <div style="max-width: 100%;">
-                    <img class="meme" src="https://c.tenor.com/9nZ5fdxEyQQAAAAC/chatting-twich-emote-xqc-asmongold-chat-tyler1.gif">
-                </div>
-                <div class="comentarioFoto">
-                    <input class="botones" type=image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDsKvKUVOqbJsF5oD4KXaBa_hoEBiYLQrY1A&usqp=CAU" >
-                    <scap class="fecha">12 comentarios</scap>
-                    <input class="botones" type=image src="https://www.nicepng.com/png/detail/119-1193195_white-left-arrow-png-png-white-arrow-left.png" style="transform:rotate(180deg);">
-                    <scap class="fecha">Compartir</scap>
-                </div>
-            </div>
-        </div>
+    </a>
+</div>
 
+<div class="subRectangulo">
+    @if( count($memes) <= 0 )
+        <p class="no-memes">No se han publicado memes en este rango de dias!</p>
+    @else
+
+        @foreach ($memes as $meme)
         <div class="rectanguloPublicacion layout">
-            <div class="sidebar" style="margin-top: 7vh">
-                <button class="botones"><span class="bx bx-up-arrow-alt"><i></i></span></button>
-                <div class="upvotes">874k</div>
-                <button class="botones"><span class="bx bx-down-arrow-alt"><i></i></span></button>
-            </div>
             <div class="body">
-                <scap class="autor">AutorRandom123</scap> <scap class="fecha">17/03/2022</scap>
-                <div style="max-width: 100%;">
-                    <img class="meme" src="https://c.tenor.com/9nZ5fdxEyQQAAAAC/chatting-twich-emote-xqc-asmongold-chat-tyler1.gif">
-                </div>
-                <div class="comentarioFoto">
-                    <input class="botones" type=image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDsKvKUVOqbJsF5oD4KXaBa_hoEBiYLQrY1A&usqp=CAU" >
-                    <scap class="fecha">12 comentarios</scap>
-                    <input class="botones" type=image src="https://www.nicepng.com/png/detail/119-1193195_white-left-arrow-png-png-white-arrow-left.png" style="transform:rotate(180deg);">
-                    <scap class="fecha">Compartir</scap>
+                <scap class="autor">{{$meme->author->username}}</scap>
+                <scap class="fecha">{{$meme->created_at}}</scap>
+                <a style="max-width: 100%;" href="{{route('meme.show', ['memeId' => $meme->id])}}">
+                    <img class="meme" src="{{asset('storage/memes/'.(string)$meme->id)}}">
+                </a>
+                <div class="comentarioFoto" style="text-decoration: none;" href="{{route('meme.show', ['memeId' => $meme->id])}}">
+                    <input class="botones" type=image src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_165452.png&f=1&nofb=1">
+                    <scap class="fecha">{{count($meme->likes)}} me gusta</scap>
+                    <input class="botones" type=image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDsKvKUVOqbJsF5oD4KXaBa_hoEBiYLQrY1A&usqp=CAU">
+                    <scap class="fecha">{{count($meme->evaluations)}} comentarios</scap>
                 </div>
             </div>
-            
         </div>
-           
-    </div>
-    
-    
+        @endforeach
+    @endif
+</div>
+
+<p class="pages">
+    Páginas:
+    @for ($i = 1; $i <= ceil($totalLength/$limit); $i++)
+        <a class="{{ $i == $page ? 'selected-page' : null}}" href="{{route('ranking', ['l' => $limit, 'p' => $i, 'r' => $range])}}">{{ $i }}</a>
+    @endfor
+</p>
 
 @endsection
